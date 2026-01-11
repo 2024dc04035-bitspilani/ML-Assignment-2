a509f49ade84e12f2da06811df2d791f6ea88fff
2024dc04035-bitspilani <2024dc04035@wilp.bits-pilani.ac.in>
2026-01-11 19:40:52 +0530

    Added Dataset Validation Script

---
+++ data/validate_dataset.py
@@ -0,0 +1,15 @@
+"""
+Dataset Validation Script
+Validates minimum requirements: 12+ features and 500+ instances
+"""
+
+import pandas as pd
+import os
+
+def validate_dataset():
+    """Validate dataset meets minimum requirements"""
+    
+    pass
+
+if __name__ == "__main__":
+    validate_dataset()
